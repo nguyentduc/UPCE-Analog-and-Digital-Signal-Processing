aa807831853cc2f927173376aecb27692d702f63
unknown <ntduc.121@gmail.com>
2019-04-28 17:26:56 +0200

    update problem3

--- week1.2.Problem3.m
+++ week1.2.Problem3.m
@@ -1,11 +1,11 @@
 % Problem 3 
 %g1(t)
-{figure(1)
+figure(1)
 fplot(@(x) 0*x, [-2,-1], 'b'), hold on
 fplot(@(x) 3.5*x+3.5, [-1,0], 'b')
-fplot(@(x) -2.5*x+1.5, [0,1], 'b')
+fplot(@(x) -2.5*x+3.5, [0,1], 'b')
 fplot(@(x) -1.5*x+2.5, [1,2], 'b')
-fplot(@(x) 0.5*x+1.5, [2,3], 'b')
+fplot(@(x) 0.5*x-1.5, [2,3], 'b')
 fplot(@(x) 0*x, [3,4], 'b')
 xlim([-2,4])
 hold off
@@ -16,9 +16,9 @@ legend({'g1(t)'},'Location','southwest')
 figure(2)
 fplot(@(x) 0*x, [-2,-1], 'b'), hold on
 fplot(@(x) 7*x.^2+6*x+3, [-1,0], 'b')
-fplot(@(x) 1.5*x.^2-1.5, [0,1], 'b')
+fplot(@(x) 1.5*x.^2-4.5*x+3, [0,1], 'b')
 fplot(@(x) -1.5*x+1.5, [1,2], 'b')
-fplot(@(x) -1.5*x.^2-1.5, [2,3], 'b')
+fplot(@(x) -1.5*x.^2-9*t-13.5, [2,3], 'b')
 fplot(@(x) 0*x, [3,4], 'b')
 xlim([-2,4])
 hold off
@@ -29,9 +29,9 @@ legend({'g2(t)'},'Location','southwest')
 figure(3)
 fplot(@(x) 3, [-2,-1], 'b'), hold on
 fplot(@(x) 2.5*x+5.5, [-1,0], 'b')
-fplot(@(x) -0.5*x+1.5, [0,1], 'b')
-fplot(@(x) 1.5*x+2.5, [1,2], 'b')
-fplot(@(x) -3.5*x+1.5, [2,3], 'b')
+fplot(@(x) -0.5*x+5.5, [0,1], 'b')
+fplot(@(x) 1.5*x+3.5, [1,2], 'b')
+fplot(@(x) -3.5*x+13.5, [2,3], 'b')
 fplot(@(x) 3, [3,4], 'b')
 xlim([-2,4])
 hold off
